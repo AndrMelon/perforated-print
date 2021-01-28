dcf33f850fbd9e2b1ecd6f6eef0922c24d6ef934
Andrew Edmonston <andrewcedmo@yahoo.com>
2021-01-27 20:07:59 -0800

    working boxes based on timer

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,39 @@
 var slider_img = document.querySelector('.slider-img');
 var images = ['1.png', '2.png', '3.png', '4.png', '5.png'];
 var i = 0;
-var clicked = false;
+var activeBox = true;
 
 function prev(){
-	if(i <= 0) i = images.length;	
-  i--;
-  clicked = true;	
-  return setImg();		 
-}
-
-function next(){
-	if(i >= images.length-1) i = -1;
-  i++;
-  clicked = true;
-	return setImg();			 
-}
-
-
-function prevDialog(){
-	if(i <= 0) initial = images.length;	
+  if (activeBox) {
+    return;
+  } else { 
+    if(initial <= 0) initial = images.length;
+  console.log(initial);	
   $('#dialog').text('');
   initial -= 1;
   individual = dialogs[initial].split('');
-  createDiag( individual );			 
+  createDiag( individual );
+  activeBox = true;
+  return setImg();
+}		
 }
 
-function nextDialog(){
-  if(i >= images.length-1) initial = -1;
+function next(){
+  console.log(activeBox);
+  if (activeBox) {
+    return;
+  } else { 
+    if(initial >= images.length-1) initial = -1;
   $('#dialog').text('');
   initial += 1;
+  console.log(initial);
   individual = dialogs[initial].split('');
   createDiag( individual );
+  activeBox = true;
+  return setImg();
 }
+}
+
 
 function clearDialogue() {
   $("#dialog").text("")
@@ -41,41 +42,40 @@ function clearDialogue() {
 	
 
 function setImg(){
-	return slider_img.setAttribute('src', "./img/"+images[i]);
+	return slider_img.setAttribute('src', "./img/"+images[initial]);
 	
 }
 
  // RPG
 
 
- var dialogs = ["Step 1: Go to the quizlet you want to print.",
-  "Step 2: Go to the quizlet you want to print.",
-   "Step 3: Go to the quizlet you want to print.", 
-   "Step 4: Go to the quizlet you want to print.",
-   "Step 5: Go to the quizlet you want to print.", ],
+ var dialogs = [" Step 1: Go to the quizlet you want to print.",
+  " Step 2: Go to the quizlet you want to print.",
+   " Step 3: Go to the quizlet you want to print.", 
+   " Step 4: Go to the quizlet you want to print.",
+   " Step 5: Go to the quizlet you want to print.", ],
     initial = 0;
     individual = dialogs[initial].split('');
 
     function createDiag ( dialog ) {
-
+      console.log("activated");
+      setTimeout(function() {
+        activeBox = false;
+      }, 3000);
       for(i = 0; i < dialog.length; i++) {
         (function(i){
             
           setTimeout(function(){
-            if(clicked) {
-              clicked = false;
-              return;
-            }
             $('#dialog').text($('#dialog').text() +   dialog[i]);
             if (i == dialog.length-1 ) {
               $('#dialog').prepend('<div id="arrow"></div>');
                 
             }
           }, 50*i);  
-        
         }(i));
         
       }
+      
     
     }
 
